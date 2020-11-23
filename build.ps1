6e423992f4b1d6b4ba3a1884f952bba26495ed89
FkThat <fkthat@yandex.ru>
2020-11-23 22:37:52 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,41 +1,39 @@
 [CmdletBinding()]
+
 param (
-    [Parameter(Position = 0)][ValidateSet('restore', 'build', 'test', 'clean', 'pack')] $Target = 'build',
-    [Parameter()][ValidateSet('Debug', 'Release')] $Config = 'Debug',
-    [Parameter()] [Switch] $ShowCoverageReport = $false
+    [Parameter(Position = 0)]
+    [ValidateSet('clean', 'restore', 'build', 'test', 'coverage', 'pack')]
+    $Target = 'build',
+
+    [Parameter()]
+    [ValidateSet('Debug', 'Release')] 
+    $Config = 'Debug'
 )
 
-if($Target -eq 'clean') {
-    git clean -dfx -e .vs -e .vscode
-    break
-}
+Push-Location $PSScriptRoot
 
-if($Target -eq 'restore') {
-    dotnet restore
-    break
-}
-
-if($Target -eq 'build') {
-    dotnet build -c $Config
-    break
-}
-
-if($Target -eq 'test') {
-    dotnet test -c $Config || break
-
-    if($ShowCoverageReport) {
-        dotnet tool restore &&
-        dotnet tool run reportgenerator `
-            -reports:**\coverage.cobertura.xml `
-            -targetdir:.coverage &&
-        Start-Process '.coverage\index.htm'
+switch($Target) {
+    'clean' {
+        git clean -dfx -e .vs -e .vscode
     }
-
-    break
+    'restore' {
+        dotnet restore
+    }
+    'build' {
+        dotnet build -c $Config
+    }
+    'test' {
+        dotnet test -c $Config
+    }
+    'coverage' {
+        dotnet tool restore &&
+        dotnet tool run reportgenerator -reports:**\coverage.cobertura.xml `
+            -targetdir:.coverage && Start-Process '.coverage\index.htm'
+    }
+    'pack' {
+        dotnet test -c $Config &&
+        dotnet pack -c $Config -o '.build' --no-build
+    }
 }
 
-if($Target -eq 'pack') {
-    dotnet test -c $Config &&
-    dotnet pack -c $Config -o '.build' --no-build
-    break
-}
+Pop-Location
