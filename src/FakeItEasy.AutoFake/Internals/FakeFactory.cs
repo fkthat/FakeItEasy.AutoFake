239829c0de3a6fd9184e93b940b029b9cc7cf357
FkThat <fkthat@yandex.ru>
2021-04-10 16:50:58 +0300

    Add FakeFactory

---
+++ src/FakeItEasy.AutoFake/Internals/FakeFactory.cs
@@ -0,0 +1,20 @@
+using System;
+using FakeItEasy.Core;
+
+namespace FakeItEasy.AutoFake.Internals
+{
+    internal class FakeFactory
+    {
+        public object? Get(Type type)
+        {
+            try
+            {
+                return Sdk.Create.Fake(type);
+            }
+            catch (FakeCreationException)
+            {
+                return null;
+            }
+        }
+    }
+}
