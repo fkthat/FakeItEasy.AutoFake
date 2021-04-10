8580fedfd6fe6dc7f1d825d17e6879b5b0c9eb29
FkThat <fkthat@yandex.ru>
2021-04-10 19:16:43 +0300

    Cleanup

--- src/FakeItEasy.AutoFake/AutoFaker.cs
+++ src/FakeItEasy.AutoFake/AutoFaker.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using FakeItEasy.AutoFake.Internals;
 using FakeItEasy.AutoFake.Parameters;
 using FakeItEasy.Core;
 
