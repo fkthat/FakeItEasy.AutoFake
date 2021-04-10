673e18da30c069021503737e3deb4b32b4403bee
FkThat <fkthat@yandex.ru>
2021-04-10 10:12:32 +0300

    Support to netstandard 1.6

--- src/FakeItEasy.AutoFake/AutoFakerConfiguration.cs
+++ src/FakeItEasy.AutoFake/AutoFakerConfiguration.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace FakeItEasy.AutoFake
