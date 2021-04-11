eb147808880dece0067078a62c24d3e9ce15f633
FkThat <fkthat@yandex.ru>
2021-04-11 16:45:09 +0300

    Extract IAutoFaker

--- src/FakeItEasy.AutoFake/AutoFaker.cs
+++ src/FakeItEasy.AutoFake/AutoFaker.cs
@@ -1,17 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using FakeItEasy.AutoFake.Fakes;
 using FakeItEasy.AutoFake.Parameters;
-using FakeItEasy.Core;
 
 namespace FakeItEasy.AutoFake
 {
     /// <summary>
     /// An auto-mocking IoC container that generates fake objects using FakeItEasy.
     /// </summary>
-    public class AutoFaker
+    public class AutoFaker : IAutoFaker
     {
         private readonly AutoFakerConfiguration _configuration;
         private readonly FakeContainer _fakeContainer;
@@ -42,29 +38,11 @@ namespace FakeItEasy.AutoFake
         }
 
         /// <summary>
-        /// Creates the instance of the <typeparamref name="T"/> type.
-        /// </summary>
-        /// <param name="parameters">
-        /// The parameters to pass to the <typeparamref name="T"/> constructor.
-        /// </param>
-        /// <returns>The created instance.</returns>
-
-        public T CreateInstance<T>(params IParameter[] parameters) =>
-            (T)CreateInstance(typeof(T), parameters);
-
-        /// <summary>
         /// Gets the service that will be provided by the AutoFake container. If a fake service of
         /// the <paramref name="type"/> wasn't created yet it will create and return a new fake.
         /// </summary>
         /// <param name="type">The type of a service.</param>
         /// <returns>The service instance.</returns>
         public object? Get(Type type) => _fakeContainer.Get(type);
-
-        /// <summary>
-        /// Gets the service that will be provided by the AutoFake container. If a fake service of
-        /// the <typeparamref name="T"/> wasn't created yet it will create and return a new fake.
-        /// </summary>
-        /// <returns>The service instance.</returns>
-        public T? Get<T>() where T : class => (T?)Get(typeof(T));
     }
 }
--- src/FakeItEasy.AutoFake/AutoFakerExtensions.cs
+++ src/FakeItEasy.AutoFake/AutoFakerExtensions.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FakeItEasy.AutoFake.Parameters;
 
 namespace FakeItEasy.AutoFake
 {
@@ -14,9 +11,33 @@ namespace FakeItEasy.AutoFake
         /// Provide a predefined instance of a service to inject later to created objects.
         /// </summary>
         /// <typeparam name="T">The service type.</typeparam>
-        /// <param name="configuration">Configuration instance.</param>
+        /// <param name="configuration"><c cref="IAutoFakerConfiguration"/> instance.</param>
         /// <param name="instance">The service instance.</param>
-        public static IAutoFakerConfiguration Use<T>(this IAutoFakerConfiguration configuration,
-            T instance) where T : class => configuration.Use(typeof(T), instance);
+        public static IAutoFakerConfiguration Use<T>(
+            this IAutoFakerConfiguration configuration, T instance) where T : class =>
+            configuration.Use(typeof(T), instance);
+
+        /// <summary>
+        /// Creates the instance of the <typeparamref name="T"/> type.
+        /// </summary>
+        /// <param name="autoFaker"><c cref="IAutoFaker"/> instance.</param>
+        /// <param name="parameters">
+        /// The parameters to pass to the <typeparamref name="T"/> constructor.
+        /// </param>
+        /// <returns>The created instance.</returns>
+        public static T CreateInstance<T>(
+            this IAutoFaker autoFaker, params IParameter[] parameters) =>
+            (T)autoFaker.CreateInstance(typeof(T), parameters);
+
+        /// <summary>
+        /// Gets the service that will be provided by the AutoFake container. If a fake service of
+        /// the <span class="typeparameter">T</span> wasn't created yet it will create and return a
+        /// new fake.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="autoFaker"><c cref="IAutoFaker"/> instance.</param>
+        /// <returns>The service instance.</returns>
+        public static T? Get<T>(this IAutoFaker autoFaker) where T : class =>
+            (T?)autoFaker.Get(typeof(T));
     }
 }
